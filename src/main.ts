bb55815145195d5fcd47c9aaf2f0039907ba7a81
Ans0nZ <151815313+Ans0nZ@users.noreply.github.com>
2025-10-10 06:22:22 +0000

    step 2: -adding a "qi" image -adding event for clicking -trying to display increment message(failed)

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import exampleIconUrl from "./dazuo.png";
 import "./style.css";
 
-let counter = 0;
-let costcounter = 0;
+let counter: number = 0;
+const UNIT = "qi";
 
 document.body.innerHTML = `
-  <p>Buttom: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>
+    <button id="increment">
+     <img src="${exampleIconUrl}" class="icon" />
+    </button>
+  </p>
+    <div id="counter">0 ${UNIT}</div>
+    <div id="message"></div>
   
 `;
 
-for (let i = 0; i < 4; i++) {
+const button = document.getElementById("increment")!;
+const counterDiv = document.getElementById("counter")!;
+const messageDiv = document.getElementById("message")!;
+
+button.addEventListener("click", () => {
   counter++;
-  costcounter++;
-}
+  counterDiv.textContent = `${counter} ${UNIT}`;
+  messageDiv.textContent = "灵气 +1";
+
+  setTimeout(() => {
+    messageDiv.textContent = "";
+  }, 800);
+});
