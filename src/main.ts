c10b776815565115255b629c06799ffb80adc04e
Ans0nZ <151815313+Ans0nZ@users.noreply.github.com>
2025-10-10 08:16:06 +0000

    step 2 complete! I added the counter and the counter icon without crushing

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,20 @@ document.body.innerHTML = `
   <div id="meditate" class="meditate">
     <img src="${exampleIconUrl}" class="icon" alt="meditator" />
     <img src="${flameIconUrl}" id="flame" class="flame" alt="flame" />
+    <div id="counter" class="counter">0 ${UNIT}</div>
   </div>
   
+  
 `;
+const meditate = document.getElementById("meditate")!;
+const flame = document.getElementById("flame") as HTMLImageElement;
+const counterDiv = document.getElementById("counter") as HTMLDivElement; //counter div
 
-for (let i = 0; i < 4; i++) {
+
+meditate.addEventListener("click", () => {
   counter++;
-  costcounter++;
-}
+  counterDiv.textContent = `${counter} ${UNIT}`;
+
+  flame.classList.add("show");
+  setTimeout(() => flame.classList.remove("show"), 600);
+});
